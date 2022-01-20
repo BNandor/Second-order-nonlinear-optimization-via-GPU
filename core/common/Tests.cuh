435e253679cf36c8d5dc12cdf78d3e5572ccfb2f
Nandor <b.nandor1998@gmail.com>
2022-01-20 19:39:27 +0200

    printing BFS order

--- core/common/Tests.cuh
+++ core/common/Tests.cuh
@@ -18,33 +18,33 @@ __global__ void testDFloatKernel(DFloat *c, unsigned *globalIndex) {
 }
 
 __global__ void functionTestsKernel(unsigned **BFS, unsigned *bfsSize, unsigned *globalIndex) {
-//    DFloat a = DFloat(2.0, atomicAdd(globalIndex, 1), globalIndex);
-//    DFloat b = DFloat(4.0, atomicAdd(globalIndex, 1), globalIndex);
-//    DFloat c = a + b;
-//    DFloat d = (c.square() + a).sqrt();
-//    DFunction f1 = DFunction();
-//    f1.BFS(d);
-//
-//    for (int i = 0; i < f1.BFSSize; i++) {
-//# if __CUDA_ARCH__ >= 200
-//        printf("%d \n", f1.BFSOrder[i]);
-//#endif
-//    }
-
-    DFloat x = DFloat(2.0, atomicAdd(globalIndex, 1), globalIndex);
-    DFloat y = DFloat(4.0, atomicAdd(globalIndex, 1), globalIndex);
-    DFloat x1 = x * y;
-    DFloat y2 = DFloat(4.0, atomicAdd(globalIndex, 1), globalIndex);
-    DFloat x2 = x1 * y2;
-    DFloat x3 = x1 + x2;
+    DFloat a = DFloat(2.0, atomicAdd(globalIndex, 1), globalIndex);
+    DFloat b = DFloat(4.0, atomicAdd(globalIndex, 1), globalIndex);
+    DFloat c = a + b;
+    DFloat d = (c.square() + a).sqrt();
     DFunction f1 = DFunction();
-    f1.BFS(x3);
+    f1.BFS(d);
 
     for (int i = 0; i < f1.BFSSize; i++) {
 # if __CUDA_ARCH__ >= 200
-        printf("%d \n", f1.parameterList[f1.BFSOrder[i]]->operation);
+        printf("%d \n", f1.BFSOrder[i]);
 #endif
     }
+//
+//    DFloat x = DFloat(2.0, atomicAdd(globalIndex, 1), globalIndex);
+//    DFloat y = DFloat(4.0, atomicAdd(globalIndex, 1), globalIndex);
+//    DFloat x1 = x * y;
+//    DFloat y2 = DFloat(4.0, atomicAdd(globalIndex, 1), globalIndex);
+//    DFloat x2 = x1 * y2;
+//    DFloat x3 = x1 + x2;
+//    DFunction f1 = DFunction();
+//    f1.BFS(x3);
+
+//    for (int i = 0; i < f1.BFSSize; i++) {
+//# if __CUDA_ARCH__ >= 200
+//        printf("%d \n", f1.parameterList[f1.BFSOrder[i]]->operation);
+//#endif
+//    }
     *BFS = f1.BFSOrder;
     *bfsSize = f1.BFSSize;
 }
