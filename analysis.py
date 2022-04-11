6cdd6dda08dadfe4246029dfd55af9dc4737b0cb
BNandor <b.nandor1998@gmail.com>
2022-04-11 18:43:23 +0300

    started FCA

--- analysis.py
+++ analysis.py
@@ -1,7 +1,15 @@
 from cv2 import GFTTDetector
 import testlogs
 import pandas as pd 
+# {'objects': ('King Arthur', 'Sir Robin', 'holy grail'), 'properties': ('human', 'knight', 'king', 'mysterious'), 'context': [(0, 1, 2), (0, 1), (3,)]}
+def mapResultRowToAttributes(row):
+    if row["final f: _gd"] > row["final f: _lbfgs"]:
+        return ['lbfgsbetter']
+    else:
+        return ['gd']
 
+def mapToFormalConcept(resultingDF):
+    return resultingDF.apply(mapResultRowToAttributes, axis=1, result_type='expand')
 testCaseColumns=['solver','problem','nodecount','edges','anchors','totaliterations','population','deIteration','distFraction','testcase']
 
 GDCases = testlogs.readLogs("./SNLP3D/problems/gridtest/csv/3D/GD/metrics/metrics-3D-random-problem-1-sample2.csv")
@@ -15,5 +23,24 @@ gdDF["optimizer"]="GD"
 print(gdDF) 
 print(lbfgsDF) 
 joinedlogs=lbfgsDF.set_index(testCaseColumns).join(gdDF.set_index(testCaseColumns),lsuffix='_lbfgs', rsuffix='_gd')
-print(joinedlogs.head(10))
-print(joinedlogs[joinedlogs["final f: _gd"] > joinedlogs["final f: _lbfgs"]].count())
+resultContext=mapToFormalConcept(joinedlogs)
+resultContext.reset_index(inplace=True)
+# resultContext=resultContext.to_dict('index')
+print(joinedlogs) 
+print(resultContext)
+
+successCount=joinedlogs.query(" `final f: _gd` >= `final f: _lbfgs`").count()['final f: _lbfgs']
+total=joinedlogs.count()['final f: _lbfgs']
+print('Percentage where LBFGS is better than GD')
+print(successCount / total)
+
+simpleSuccessPerDistFrac=joinedlogs.query('deIteration == 0 and `final f: _gd` >= `final f: _lbfgs`').groupby(['distFraction']).count()['final f: _gd']
+simpleTotalsPerDistFrac=joinedlogs.query('deIteration == 0 ').groupby(['distFraction']).count()['final f: _gd']
+deSuccessPerDistFrac=joinedlogs.query('deIteration > 0 and `final f: _gd` >= `final f: _lbfgs`').groupby(['distFraction']).count()['final f: _gd']
+deTotalsPerDistFrac=joinedlogs.query('deIteration > 0').groupby(['distFraction']).count()['final f: _gd']
+print('No DE: Percentage where LBFGS is better than GD per distFraction ')
+print(simpleSuccessPerDistFrac/simpleTotalsPerDistFrac)
+print('DE: Percentage where LBFGS is better than GD per distFraction ')
+print(deSuccessPerDistFrac/deTotalsPerDistFrac)
+
+
